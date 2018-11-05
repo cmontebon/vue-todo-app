75caa42dd1125cdaf5828443ca99810f7c715684
Christian Philip Montebon <cmontebon@innovuze.com>
2018-11-05 11:56:52 +0800

    Commit initial files.

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Vue To-Do App</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 750px;
+  margin:auto;
+  padding: 20px;
+  height:750px;
+  background-color: #fff;
+}
+
+body {
+  background-color: #4ac08e;
+}
+</style>
